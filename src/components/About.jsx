60fe2c7a178411d134f0b2677fd54b6b0f00393d
Imani Nakhama Lunjala <lunjalaimani@gmail.com>
2026-05-11 07:10:25 +0300

    about corrected

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,12 @@
-<aside>
-    <img src="{image}" alt="blog logo" />
-    <p>Welcome to our app!</p>
-</aside>
+function About({ image}){
+    return (
+        <aside>
+            <img src={image} alt="blog logo" />
+            <p>Welcome to our app!</p>
+        </aside>
+    
+    );
+}
+
+
 export default About;
